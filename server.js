78583a3ad276c22c9515707d1ae55380104cab4c
Gemycode <abdallagamalph@gmail.com>
2025-06-16 10:31:06 +0300

    last

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
